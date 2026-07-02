f844511f5b9d551b97748e6535f1bfecca9397df
MAHI-g431 <gipsya42@gmail.com>
2026-07-02 22:21:50 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Protfolio(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+
+		fmt.Println("New form submission")
+		fmt.Println("Name:", name)
+		fmt.Println("Email:", email)
+		fmt.Println("_______________________________________________________________________________________")
+		http.ServeFile(w, r, "form.html")
+		return
+	}
+	http.ServeFile(w, r, "go.html")
+
+}
+func main() {
+	http.HandleFunc("/", Protfolio)
+	http.Handle("/tick.png", http.FileServer(http.Dir(".")))
+	fmt.Println("Server running at http://localhost:3000")
+	http.ListenAndServe(":3000", nil)
+}
